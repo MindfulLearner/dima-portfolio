e76debd77cacb3418694c9250cf8cb7119b73b19
MindfulLearner <dimajosh1009@gmail.com>
2024-10-24 12:03:44 +0000

    header terminato

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,57 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft, faArrowRight, faSearch, faGear, faSignOut, faSignIn } from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
-  return <div className="header h-[34px] bg-blue-500">header</div>;
+  return (
+    <div className="flex justify-between items-center header h-[34px] bg-myVSCodeColor">
+      {/* parte sinistra con controllo per aprire il file explorer  con icona e i controlli per aprire i file */}
+      <div className="flex w-1/3 gap-5 items-center pl-2">
+        <div className="w-[20px] h-[20px]">
+          <img className="w-full h-full" src="/icons/png-fungo-icon.png" alt="" />
+        </div>
+        <div>
+          <ul className="flex gap-5 text-white">
+            <li>file</li>
+            <li>edit</li>
+            <li>select</li>
+            <li>view</li>
+            <li>go</li>
+            <li>run</li>
+            <li>terminal</li>
+            <li>help</li>
+          </ul>
+        </div>
+      </div>
+
+      {/* parte centrale searchbar con controllo per andare a sinistra e a destra */}
+      <div className="flex w-1/3 gap-5 justify-center items-center bg-searchbarColor rounded-md border border-neutralColor">
+        <div className="flex text-white gap-3">
+          <FontAwesomeIcon icon={faArrowLeft} />
+          <FontAwesomeIcon icon={faArrowRight} />
+        </div>
+        <div className="flex gap-3 items-center text-white">
+          <FontAwesomeIcon icon={faSearch} />
+          <p>dima-portfolio [SSH: dima@dima-portfolio]</p>
+        </div>
+      </div>
+
+      {/* parte destra con controllo per uscire dal vscode */}
+      <div className="flex justify-end w-1/3 gap-4 pr-3">
+        <div className="flex gap-3 text-white text-lg">
+          <FontAwesomeIcon icon={faGear} />
+          <FontAwesomeIcon icon={faGear} />
+          <FontAwesomeIcon icon={faGear} />
+          <FontAwesomeIcon icon={faGear} />
+        </div>
+        <div className="flex gap-5 text-white">
+          <FontAwesomeIcon icon={faSignOut} />
+          <FontAwesomeIcon icon={faSignIn} />
+          <FontAwesomeIcon icon={faSignOut} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // VSCode color
+        myVSCodeColor: '#1f1f1f', // color of the header
+        searchbarColor: '#313131', // color of the searchbar
+        // VSCode color
+        accentColor: '#E3342F', // color of the accent
+        neutralColor: '#6C757D', // color of the neutral
+        successColor: '#38C172', // color of the success
+        dangerColor: '#E3342F', // color of the danger
+        warningColor: '#FFAA00', // color of the warning
+        infoColor: '#6CB2EB', // color of the info
+      },
+    },
   },
+  variants: {},
   plugins: [],
 }
