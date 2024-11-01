39770a92a710eb8f36ede106c48120d8fac25f6c
MindfulLearner <dimajosh1009@gmail.com>
2024-11-01 13:34:01 +0000

    added working click slide

--- src/components/TextEditorSection/TextEditorPanel4.tsx
+++ src/components/TextEditorSection/TextEditorPanel4.tsx
@@ -1,6 +1,9 @@
 //text editor panel 4
 import React, { useState } from "react";
 import carouselWork from "../data/CarouselWork";
+
+
+
 function TextEditorPanel4() {
   const [isHoveredSelectTab, setIsHoveredSelectTab] = useState(false);
 
@@ -10,6 +13,17 @@ function TextEditorPanel4() {
   function handleMouseOutSelectTab() {
     setIsHoveredSelectTab(false);
   }
+
+
+const handleClickScrollTo = (index: number) => {
+  const section = document.getElementById(`${index}`);
+  console.log(section);
+  if (section) {
+    section.scrollIntoView({ behavior: "smooth" });
+  }
+}
+
+
   return (
     <div className="h-1/2 bg-backgroundTextEditor1 border-b border-l border-r border-borderColor">
       <div className="shadow-bottom w-full flex  bg-emptybarColor shadow-lg">
@@ -52,35 +66,43 @@ function TextEditorPanel4() {
       <div className="h-[calc(100%-31px)] w-full overflow-y-scroll no-scrollbar">
         {/* questo sara in position fixed */}
         {/* da sistemare con GRID */}
-          <div className="fixed grid grid-cols-12 pt-16 w-full">
+          <div className="fixed grid grid-cols-12 pt-16 w-full ml-10">
             {carouselWork.map((item: any, index: number) => (
-              <div key={index} className="font-quicksand text-gray-300 font-bold col-start-2">
-                {item.title}
+              <div key={index} className="font-quicksand text-gray-300 font-bold col-start-1">
+                <a className="hover:text-gray-500 cursor-pointer" onClick={() => handleClickScrollTo(index)}>{item.title}</a>
               </div>
           ))}
         </div>
-
-          <div className="h-[300px] grid grid-cols-12 gap-4 mr-10 pt-16">
+        {/* section of works */}
+          <div className="h-[300px] grid grid-cols-12 gap-4 mr-5 pt-16" id="0">
+            <div className="col-start-3 col-end-5">
+              <div className="font-quicksand text-gray-300 font-bold">
+               <img src={carouselWork[0].icon} alt={carouselWork[0].title} />
+              </div>
+            </div>
             <div className="col-start-5 col-end-8">
-              <img src={carouselWork[0].image[1]} alt={carouselWork[0].title} />
+              <img className="shadow-xl" src={carouselWork[0].image[1]} alt={carouselWork[0].title} />
             </div>
             <div className="col-start-8 col-end-10">
-              <img src={carouselWork[0].image[3]} alt={carouselWork[0].title} />
+              <img className="shadow-xl" src={carouselWork[0].image[3]} alt={carouselWork[0].title} />
             </div>
             <div className="col-start-10 col-end-13">
-              <img src={carouselWork[0].image[2]} alt={carouselWork[0].title} />
+              <img className="shadow-xl" src={carouselWork[0].image[2]} alt={carouselWork[0].title} />
             </div>
           </div>
 
-          <div className="h-[300px] grid grid-cols-12 gap-4 mr-10 pt-16">
+          <div className="h-[300px] grid grid-cols-12 gap-4 mr-10 pt-16" id="1">
+            <div className="col-start-3 col-end-5">
+              <img className="rounded-xl bg-gray-100" src={carouselWork[1].icon} alt={carouselWork[1].title} />
+            </div>
             <div className="col-start-5 col-end-8">
-              <img src={carouselWork[0].image[1]} alt={carouselWork[0].title} />
+              <img className="shadow-xl" src={carouselWork[1].image[1]} alt={carouselWork[1].title} />
             </div>
             <div className="col-start-8 col-end-10">
-              <img src={carouselWork[0].image[3]} alt={carouselWork[0].title} />
+              <img className="shadow-xl" src={carouselWork[1].image[3]} alt={carouselWork[1].title} />
             </div>
             <div className="col-start-10 col-end-13">
-              <img src={carouselWork[0].image[2]} alt={carouselWork[0].title} />
+              <img className="shadow-xl" src={carouselWork[1].image[2]} alt={carouselWork[1].title} />
             </div>
           </div>
 
