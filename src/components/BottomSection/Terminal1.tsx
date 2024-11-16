cad76f0fb52b130328aa124f6f9357d526190f3b
MindfulLearner <dimajosh1009@gmail.com>
2024-11-16 16:24:07 +0000

    feat/definedTerminalArrais
    put on files ideas + API calls idea and .env create with keys have to define logic now

---
+++ src/components/BottomSection/Terminal1.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Terminal1() {
+  return (
+    <div className="outline outline-1 outline-red-500 w-full h-[calc(100%-2px)]">
+      <div>terminale1</div>
+    </div>
+  );
+}
+
+export default Terminal1;
---
+++ src/components/BottomSection/Terminal2.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Terminal2() {
+  return <div>Terminal2</div>;
+}
+
+export default Terminal2;
