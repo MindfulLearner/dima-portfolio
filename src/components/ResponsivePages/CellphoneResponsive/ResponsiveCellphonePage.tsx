7257a6f0bd1509af7f69ae7010ee48e9cbc1c319
MindfulLearner <dimajosh1009@gmail.com>
2025-06-29 03:02:53 +0200

    changes cleanup

--- src/components/ResponsivePages/CellphoneResponsive/ResponsiveCellphonePage.tsx
+++ src/components/ResponsivePages/CellphoneResponsive/ResponsiveCellphonePage.tsx
@@ -1,12 +1,24 @@
 import React from "react";
+import Header from "../../Header";
+import Footer from "../../Footer";
 import { NavBarForPanel } from "./components/EditorSection/NavBarForPanel";
 
-const MainContainer: React.FC = () => {
+const ResponsiveCellphonePage: React.FC = () => {
     return (
-        <div className="swag">
-        
+        <div className="flex flex-col h-screen">
+            <Header />
+            <main className="flex-1 overflow-auto">
+                <div className="p-4">
+                    <NavBarForPanel />
+                    <div className="mt-4 p-4 bg-gray-100 rounded-lg">
+                        <h2 className="text-lg font-semibold mb-2">MOBILE</h2>
+                        <p>MOBULE</p>
+                    </div>
+                </div>
+            </main>
+            <Footer />
         </div>
     );
 };
 
-export default MainContainer;
+export default ResponsiveCellphonePage;
