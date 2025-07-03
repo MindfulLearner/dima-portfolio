7a3a0bb78400a97d16ee37692dccd6ede0993638
MindfulLearner <dimajosh1009@gmail.com>
2025-07-03 11:34:03 +0200

    cleanup

--- src/components/ResponsivePages/CellphoneResponsive/ResponsiveCellphonePage.tsx
+++ src/components/ResponsivePages/CellphoneResponsive/ResponsiveCellphonePage.tsx
@@ -3,8 +3,10 @@ import React from "react";
 const ResponsiveCellphonePage: React.FC = () => {
     return (
         <div className="flex flex-col h-screen">
-            <main className="flex-1 overflow-auto">
-              swaW
+            <main className="bg-black flex-1 overflow-auto">
+                <div className="text-white text-center text-2xl font-bold h-full flex items-center justify-center">
+                    working in progress, this wont have some features for now
+                </div>
             </main>
         </div>
     );
